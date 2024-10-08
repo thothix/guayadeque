54b06599e5d39ca3bc898a30975c63fd68e71191
Bryon Roche <kain@kain.org>
2024-10-08 05:57:24 -0500

    Use a better RNG for better shuffle/smart playlists.
      The default generator guayadeque has used, rand() does not provide
      a good numeric distribution, and the quality of playlist shuffle and
      dynamic playlists suffers as a result.  Add optional use of the
      random() RNG instead.  (This could be made automatic, but I figure it
      should be manual for at least a time, in case someone somehow is
      running guayadeque on a system that can't satisfy POSIX_C_SOURCE, and
      it does change the observed behavior of the RNG.)

--- src/misc/Utils.h
+++ src/misc/Utils.h
@@ -43,8 +43,13 @@ namespace Guayadeque {
     #define guLogError      wxLogError
 #endif
 
+#ifdef __POSIX_C_SOURCE
+#define guRandomInit() (srandom( time( NULL ) ))
+#define guRandom(x) (random() % x)
+#else
 #define guRandomInit() (srand( time( NULL ) ))
 #define guRandom(x) (rand() % x)
+#endif
 
 #define guDEFAULT_BROWSER_USER_AGENT    wxT( "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/55.0.2883.87 Chrome/55.0.2883.87 Safari/537.36" )
 
