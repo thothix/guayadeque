5a8ae9fb43ff599ab22656e89d0d74ff80849a78
Thothix <thothix@protonmail.com>
2024-11-11 15:24:23 -0300

    Added missing license header

--- src/ui/cover/CoverWindow.h
+++ src/ui/cover/CoverWindow.h
@@ -1,3 +1,22 @@
+/*
+   Copyright (C) 2008-2023 J.Rios <anonbeat@gmail.com>
+   Copyright (C) 2024 Tiago T Barrionuevo <thothix@protonmail.com>
+
+   This file is part of Guayadeque Music Player.
+
+   Guayadeque is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Guayadeque is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Guayadeque. If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef __COVERWINDOW_H__
 #define __COVERWINDOW_H__
 
