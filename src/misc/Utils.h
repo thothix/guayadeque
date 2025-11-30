c6187a2fc06377c818bd33c46a94add31fca638f
Thothix <thothix@protonmail.com>
2025-11-30 19:37:59 -0300

    Add utility functions JoinFromArrayInt(), SplitToArrayInt() and GetSuperscriptNumber()

--- src/misc/Utils.h
+++ src/misc/Utils.h
@@ -122,6 +122,9 @@ int wxCMPFUNC_CONV CompareFileTimeA( guFileItem ** item1, guFileItem ** item2 );
 int wxCMPFUNC_CONV CompareFileTimeD( guFileItem ** item1, guFileItem ** item2 );
 int wxCMPFUNC_CONV CompareFileTypeA( guFileItem ** item1, guFileItem ** item2 );
 int wxCMPFUNC_CONV CompareFileTypeD( guFileItem ** item1, guFileItem ** item2 );
+wxString JoinFromArrayInt(const wxArrayInt &intArray, const wxChar &delimiter = ',');
+wxArrayInt SplitToArrayInt(const wxString &data, const wxChar &delimiter = ',');
+wxString GetSuperscriptNumber(int number);
 
 std::mt19937        guSRandom();
 extern std::mt19937 rng_default_generator;
